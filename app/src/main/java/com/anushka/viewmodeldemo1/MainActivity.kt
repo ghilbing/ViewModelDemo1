06a60e8595ebf1f2aedeface6cf20a88c07f6db7
Gretel Hilbing <ghilbing@gmail.com>
2021-09-20 14:33:02 -0700

    ViewModel with DataBinding in xml file

--- app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
+++ app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -15,11 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.myViewModel = viewModel
         viewModel.totalData.observe(this, Observer {
             binding.countText.text = it.toString()
         })
-        binding.button.setOnClickListener {
-            viewModel.updateCount()
-        }
+
     }
 }
