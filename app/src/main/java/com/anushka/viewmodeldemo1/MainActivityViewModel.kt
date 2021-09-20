025f6aef8a6f06bd6ae26e1a5d12c8ed81355c37
Gretel Hilbing <ghilbing@gmail.com>
2021-09-20 14:23:29 -0700

    LiveData

--- app/src/main/java/com/anushka/viewmodeldemo1/MainActivityViewModel.kt
+++ app/src/main/java/com/anushka/viewmodeldemo1/MainActivityViewModel.kt
@@ -1,16 +1,18 @@
 package com.anushka.viewmodeldemo1
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
-    private var count = 0
+    private var count = MutableLiveData<Int>()
 
-    fun getCurrentCount(): Int{
-
-        return count
+    val totalData : LiveData<Int>
+    get() = count
+    init {
+        count.value = 0
     }
-
-    fun getUpdatedCount(): Int{
-        return ++count
+    fun updateCount(){
+        count.value = (count.value)?.plus(1)
     }
 }
